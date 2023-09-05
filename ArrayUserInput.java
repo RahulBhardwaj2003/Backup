6ddccd35222ed3ac33b4608ce0599a845f876970
RahulBhardwaj2003 <136234696+RahulBhardwaj2003@users.noreply.github.com>
2023-09-05 16:54:00 +0530

    Add files via upload

---
+++ ArrayUserInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayUserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+        int n = sc.nextInt();
+        int[] array = new int[100];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println("Array elements are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
