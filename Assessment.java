11a383c521b5ceb4a5ef7a1cd4632f71f62a72c7
RahulBhardwaj2003 <136234696+RahulBhardwaj2003@users.noreply.github.com>
2023-09-14 13:42:27 +0530

    Add files via upload

---
+++ Assessment.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+public class Assessment {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number of students: ");
+        int stu = sc.nextInt();
+
+        for(int i=0; i<stu; i++){
+            System.out.println("enter the uid of the student: ");
+        int num = sc.nextInt();
+        System.out.println("Enter the name of the student: ");
+        String s = sc.next();
+        System.out.println("Enter the section of the student: ");
+        String se = sc.next();
+        }
+
+        // System.out.println("enter the uid of the student: ");
+        // int num = sc.nextInt();
+        // System.out.println("Enter the name of the student: ");
+        // String s = sc.next();
+        // System.out.println("Enter the section of the student: ");
+        // String se = sc.next();
+
+        // System.out.println("enter the uid of the student: ");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter the name of the student: ");
+        // String s1 = sc.next();
+        // System.out.println("Enter the section of the student: ");
+        // String se1 = sc.next();
+
+        // System.out.println("enter the uid of the student: ");
+        // int num2 = sc.nextInt();
+        // System.out.println("Enter the name of the student: ");
+        // String s2 = sc.next();
+        // System.out.println("Enter the section of the student: ");
+        // String se2 = sc.next();
+
+        // System.out.println("enter the uid of the student: ");
+        // int num3 = sc.nextInt();
+        // System.out.println("Enter the name of the student: ");
+        // String s3 = sc.next();
+        // System.out.println("Enter the section of the student: ");
+        // String se3 = sc.next();
+        
+        System.out.println("enter student number whose details have to be fetched: ");
+        int j = sc.nextInt();
+
+       switch(j) {
+        case
+        System.out.println("Name= " +s);
+        System.out.println("UID= " +num);
+        System.out.println("Section= " +se);
+        break;
+       
+    //    case 2: 
+    //     System.out.println("Name= " +s1);
+    //     System.out.println("UID= " +num1);
+    //     System.out.println("Section= " +se1);
+    //     break;
+
+    //     case 3: 
+    //     System.out.println("Name= " +s2);
+    //     System.out.println("UID= " +num2);
+    //     System.out.println("Section= " +se2);
+    //     break;
+       
+    //     case 4: 
+    //     System.out.println("Name= " +s3);
+    //     System.out.println("UID= " +num3);
+    //     System.out.println("Section= " +se3);
+    //     break;
+    //    }
+    }
+}
