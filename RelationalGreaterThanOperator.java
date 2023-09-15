610e2ba2484dcf3c008dd7218ee2218e278c7d89
RahulBhardwaj2003 <136234696+RahulBhardwaj2003@users.noreply.github.com>
2023-09-16 01:00:50 +0530

    Add files via upload

---
+++ RelationalGreaterThanOperator.java
@@ -0,0 +1,13 @@
+public class RelationalGreaterThanOperator {
+    public static void main(String[] args) {
+        int var1=57, var2= 46, var3=99;
+
+        System.out.println("var1= " +var1);
+        System.out.println("var2= " +var2);
+        System.out.println("var3= " +var3);
+
+        System.out.println("var1 >= var2 " +(var1 > var2));
+        System.out.println("var1 >= var3 " +(var1 > var3));
+        System.out.println("var2 >= var3 " +(var2 > var3));
+    }
+}
